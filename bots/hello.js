cb671c0d0df6744ce64b56a65da3e2698a79535b
Ameesh Kapoor <ameesh@gmail.com>
2014-12-19 14:07:41 -0500

    Fixing bug sending multiple messages

--- bots/hello.js
+++ bots/hello.js
@@ -23,7 +23,7 @@ module.exports = {
 		var status = 200;
 		var hookUrl = request.query.url;
 		var requestedFeed = request.payload.text;
-		var rand = Math.floor((Math.random() * 5));
+		var rand = Math.floor((Math.random() * 10)) + 1;
 		var counter = 0;
 		var feed = requestedFeed == 'deal' ? 'http://feeds.feedburner.com/SlickdealsnetForums-9' : 'http://feeds.feedburner.com/TechCrunchIT';
 		// If you would like to change the name on a per-response basis,
@@ -40,12 +40,12 @@ module.exports = {
 						while (item = stream.read()){
 								// Each 'readable' event will contain 1 article
 								// Add the article to the list of episodes
+								counter++;
 								if (counter == rand) {
-  								sendPost(hookUrl, {'text': '<' + item.link + '>'});
-  								reply(JSON.stringify({'text': '<' + item.link + '>'})).code(status);
+  								sendPost(hookUrl, {'text': item.link});
+  								reply(JSON.stringify({'text': item.link})).code(status);
   								return;
   						  }
-  							counter++;
 						}
 				})
 			});
